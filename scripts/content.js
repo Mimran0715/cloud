2f7d0b0a51358e3634e4d7fb5fe55200eabe4bfc
Mimran0715 <maleeha.imranm@gmail.com>
2024-04-14 16:12:34 -0700

    added scripts/content.js

---
+++ scripts/content.js
@@ -0,0 +1,7 @@
+const article = document.querySelector("article");
+
+// `document.querySelector` may return null if the selector doesn't match anything.
+if (article) {
+  // do something 
+  // no comments when push to chrome store
+}
